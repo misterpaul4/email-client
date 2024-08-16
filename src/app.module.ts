36a57f3f6d89a7b3e31fae3171ceb9788cd4fb2d
Paul Ajuizeogu <ajuzepaul@gmail.com>
2024-08-16 03:50:27 +0100

    Fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Make the configuration available globally
+    }),
+    TypeOrmModule.forRoot({
+      useFactory: () => ({
+        type: "postgres",
+        host: process.env.DATABASE_HOST,
+        port: parseInt(process.env.DATABASE_PORT, 10),
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        synchronize: true,
+        autoloadEntities: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
