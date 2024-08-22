99a3e7655739396be6cd07968c5c0e357291274e
paul <paul.a@mblhightech.com>
2024-08-22 17:59:34 +0100

    Oauth2 setup for UI

--- apps/email-client-ui/src/app/app.tsx
+++ apps/email-client-ui/src/app/app.tsx
@@ -1,7 +1,35 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
 export function App() {
+  const googleUrl = new URL('https://accounts.google.com/o/oauth2/auth');
+
+  googleUrl.searchParams.append(
+    'client_id',
+    import.meta.env.VITE_GOOGLE_CLIENT_ID
+  );
+
+  googleUrl.searchParams.append(
+    'redirect_uri',
+    import.meta.env.VITE_GOOGLE_REDIRECT_URI
+  );
+
+  googleUrl.searchParams.append('scope', 'https://mail.google.com/');
+  googleUrl.searchParams.append('response_type', 'code');
+
+  const { hash } = useLocation();
+
+  useEffect(() => {
+    // validate hash
+    // display loader
+    // token exchange request
+  }, [hash]);
+
+
   return (
     <div>
-     Home
+      Home
+      <a href={googleUrl.toString()}>Click Me</a>
     </div>
   );
 }
